a8c6928a5563f65436b15992677c72673df99869
Ryan Hitchman <hitchmanr@gmail.com>
2020-11-20 17:06:42 -0800

    shade block sides like official minecraft, add fog

--- src/cube_fragment.glsl
+++ src/cube_fragment.glsl
@@ -14,5 +14,12 @@ out vec4 outColor;
 void main()	{
     vec4 color = vec4( vColor ) * texture(atlas, vTexCoord);
     if (color.a == 0.0) discard;
-    outColor = vec4(color.rgb * (0.85+.2*dot(vNormal, normalize(vec3(10,5,2)))), color.a);
+    // OFFICIAL MINECRAFT:
+    //  *0.8 on the Z axis faces, by *0.6 on the X axis faces, and by *0.5 on the bottom face
+    outColor = mix(
+        vec4(color.rgb *
+            max(0.5, dot(vec3(abs(vNormal.x), vNormal.y, abs(vNormal.z)), vec3(0.6,1.0,0.8)))
+            , color.a),
+        vec4(.722, .855, 1.0, 1.0),
+        min(1.0, (gl_FragCoord.z / gl_FragCoord.w) / 3000.0));
 }
