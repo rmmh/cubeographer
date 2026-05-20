39a2ca79692c8875e22925c6ae46b91430b5ab9e
Ryan Hitchman <hitchmanr@gmail.com>
2026-05-20 11:10:06 -0600

    improve fake region sorting/rendering

--- go/region/region_fakes.go
+++ go/region/region_fakes.go
@@ -2,6 +2,9 @@ package region
 
 import (
 	"fmt"
+	"math/bits"
+	"sort"
+	"strings"
 
 	"github.com/rmmh/cubeographer/go/render"
 )
@@ -11,6 +14,16 @@ func FakeReadRegion(path string, bm *BlockMapper, wanted []int) ([]ChunkDatum, e
 
 	if path != "r.0.0.mca" {
 		return cdata, nil
+	}
+
+	set := func(x, y, z int, b uint16, s render.Stateval) {
+		if x < 0 || x >= 512 || z < 0 || z >= 512 {
+			panic(fmt.Sprintf("coord out of bounds (%d,%d)", x, z))
+		}
+		chunk := &cdata[x>>4+(z>>4)*32]
+		o := (x % 16) + (z%16)*16 + (y%16)*256
+		chunk.Blocks[y/16][o] = b
+		chunk.BlockState[y/16][o] = s
 	}
 
 	for cn := 0; cn < 1024; cn++ {
@@ -24,9 +37,6 @@ func FakeReadRegion(path string, bm *BlockMapper, wanted []int) ([]ChunkDatum, e
 			for i := range 4096 {
 				nb[i] = bm.NameToNid["minecraft:air"]
 			}
-			for j := 0; j < 256; j++ {
-				nb[j+256] = bm.NameToNid["minecraft:grass_block"]
-			}
 			nblocks = append(nblocks, nb)
 			nstates = append(nstates, ns)
 		}
@@ -39,33 +49,95 @@ func FakeReadRegion(path string, bm *BlockMapper, wanted []int) ([]ChunkDatum, e
 		}
 	}
 
-	set := func(x, y, z int, b uint16, s render.Stateval) {
-		if x < 0 || x >= 512 || z < 0 || z >= 512 {
-			panic(fmt.Sprintf("coord out of bounds (%d,%d)", x, z))
+	grass := bm.NameToNid["minecraft:grass_block"]
+
+	for x := range 256 {
+		for z := range 256 {
+			set(x, 1, z, grass, 0)
 		}
-		chunk := &cdata[x>>4+(z>>4)*32]
-		o := (x % 16) + (z%16)*16 + (y%16)*256
-		chunk.Blocks[y/16][o] = b
-		chunk.BlockState[y/16][o] = s
 	}
 
 	bx := 32
 	bz := 32
 
-	for layer := 0; layer < int(render.NumRenderLayers); layer++ {
-		qualName := []string{
-			"gold_block", "diamond_block", "emerald_block", "dirt", "copper_block", "iron_block", "stone"}[layer]
+	layerMap := map[int][]int{}
+
+	layerBlocks := [][]string{{"MISSING"}}
+
+	for b := 1; b < len(bm.NidToName); b++ {
+		layerMask := 0
+		for _, ls := range bm.Layer[b] {
+			for _, l := range ls {
+				layerMask |= 1 << l
+			}
+		}
+		if layerMask == 0 {
+			// fmt.Println("layerMask == 0", bm.NidToName[b], bm.Layer[b])
+			layerBlocks[0] = append(layerBlocks[0], bm.NidToName[b])
+			continue
+		}
+		layerMap[layerMask] = append(layerMap[layerMask], b)
+	}
+
+	layerMasks := []int{}
+	for layerMask := range 1 << render.NumRenderLayers {
+		if len(layerMap[layerMask]) == 0 {
+			continue
+		}
+		layerMasks = append(layerMasks, layerMask)
+	}
+	sort.Slice(layerMasks, func(i, j int) bool {
+		// sort order: single layer masks (other than FALLBACK), then multi-layer masks without FALLBACK,
+		// then multi-layer masks with FALLBACK, then FALLBACK
+
+		// Rule 3: Pure fallback always goes to the very end
+		fallbackMask := 1 << render.LayerCubeFallback
+		iIsPureFallback := layerMasks[i] == fallbackMask
+		jIsPureFallback := layerMasks[j] == fallbackMask
+		if iIsPureFallback != jIsPureFallback {
+			return jIsPureFallback // The one that is NOT pure fallback comes first
+		}
+
+		// Rule 1: Single layers come first
+		iIsSingleLayer := (layerMasks[i] & (layerMasks[i] - 1)) == 0
+		jIsSingleLayer := (layerMasks[j] & (layerMasks[j] - 1)) == 0
+		if iIsSingleLayer != jIsSingleLayer {
+			return iIsSingleLayer
+		}
+
+		// Rule 2: Multi-layers without fallback come before multi-layers with fallback
+		iHasFallback := (layerMasks[i] & fallbackMask) != 0
+		jHasFallback := (layerMasks[j] & fallbackMask) != 0
+		if iHasFallback != jHasFallback {
+			return jHasFallback // The one WITHOUT fallback comes first
+		}
+
+		return layerMasks[i] < layerMasks[j]
+	})
+
+	for _, layerMask := range layerMasks {
+		layer := bits.TrailingZeros(uint(layerMask))
+
+		// by default, we use a minecraft block that is two mixed metals:
+		qualName := "weathered_cut_copper"
+		if layerMask&(layerMask-1) == 0 { // power of 2 == block represented by single layer
+			qualName = []string{
+				"gold_block", "diamond_block", "emerald_block", "dirt", "copper_block", "andesite"}[layer]
+		} else if (layerMask & (1 << render.LayerCubeFallback)) != 0 {
+			qualName = "cobblestone"
+		}
+
+		layers := []string{}
+		for i := range render.LayerNames {
+			if (layerMask & (1 << i)) != 0 {
+				layers = append(layers, render.LayerNames[i])
+			}
+		}
+		layerBlocks = append(layerBlocks, []string{strings.Join(layers, "_")})
+
 		qualBlock := bm.NameToNid["minecraft:"+qualName]
 
-		for b := 1; b < len(bm.NidToName); b++ {
-			if len(bm.Layer[b]) == 0 || len(bm.Layer[b][0]) == 0 {
-				continue
-			}
-			layerIdx := bm.Layer[b][0][0]
-			if int(layerIdx) != layer {
-				continue
-			}
-
+		for _, b := range layerMap[layerMask] {
 			ns := []int{}
 			seen := map[string]bool{}
 			maxState := int(bm.nidToSmap[b].Max())
@@ -81,6 +153,7 @@ func FakeReadRegion(path string, bm *BlockMapper, wanted []int) ([]ChunkDatum, e
 				seen[key] = true
 				ns = append(ns, i)
 			}
+			layerBlocks[len(layerBlocks)-1] = append(layerBlocks[len(layerBlocks)-1], bm.NidToName[b])
 			nl := len(ns)/6 + 1
 			if bx+nl >= 220 {
 				bx = 32
@@ -92,6 +165,14 @@ func FakeReadRegion(path string, bm *BlockMapper, wanted []int) ([]ChunkDatum, e
 			}
 			bx += nl
 			bx += 2
+		}
+	}
+
+	for _, lbs := range layerBlocks {
+		if len(lbs) > 3 {
+			fmt.Printf("%s (%d): %s ... %s\n", lbs[0], len(lbs)-1, lbs[1], lbs[len(lbs)-1])
+		} else {
+			fmt.Printf("%s: %s\n", lbs[0], strings.Join(lbs[1:], " "))
 		}
 	}
 
